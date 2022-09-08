70cea33f0d71e1d995ba0246702d13612819782d
Grant <grantdewaay@gmail.com>
2022-09-07 23:00:02 -0500

    cont

---
+++ Experiments/gdewaay/demo/src/main/java/cs309/TestData.java
@@ -0,0 +1,16 @@
+package cs309;
+
+public class TestData {
+	
+	private String message;
+	
+	public TestData() {}
+	
+	public TestData(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ Experiments/gdewaay/demo/src/test/java/cs309/TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package cs309;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
