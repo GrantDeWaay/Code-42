c1ebbc682ca31b11897bb19500c3121e4b3ac4e8
abowen <abowen@iastate.edu>
2022-09-08 10:32:56 -0500

    Setup keyboard project and finish navigation exp

---
+++ Experiments/abowen/exp1/app/src/main/java/edu/iastate/code42/ui/home/HomeFragment.java
@@ -0,0 +1,45 @@
+package edu.iastate.code42.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.navigation.NavigationView;
+
+import edu.iastate.code42.R;
+import edu.iastate.code42.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        NavController navController = Navigation.findNavController(getView());
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ Experiments/abowen/exp2/app/src/main/java/edu/iastate/keyboard/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.iastate.keyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
