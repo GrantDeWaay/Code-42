63be58532493225ca7ff15639d5319a4480df82b
abowen <abowen@iastate.edu>
2022-09-16 22:44:07 -0500

    Test Volley with Postman

---
+++ Experiments/abowen/volleyTest/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -0,0 +1,9 @@
+package com.example.sumon.androidvolley.utils;
+
+public class Const {
+	public static final String URL = "https://d2cd5b61-aeb9-49b1-875f-ef6db22a77eb.mock.pstmn.io/";
+	public static final String URL_JSON_OBJECT = "user/";
+	public static final String URL_JSON_ARRAY = "classes/";
+	public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
+	public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+}
