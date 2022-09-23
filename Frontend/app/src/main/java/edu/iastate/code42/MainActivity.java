b0ea1dbab75424be1b11f6140c4a4409f8573e82
abowen <abowen@iastate.edu>
2022-09-23 14:28:34 -0500

    Create login user interface and setup skeleton code for Volley authentication

--- Frontend/app/src/main/java/edu/iastate/code42/MainActivity.java
+++ Frontend/app/src/main/java/edu/iastate/code42/MainActivity.java
@@ -2,13 +2,76 @@ package edu.iastate.code42;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import edu.iastate.code42.utils.Const;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText password;
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences userSession = getSharedPreferences("user_sessions", MODE_PRIVATE);
+        SharedPreferences appSetting = getSharedPreferences("app_setting", MODE_PRIVATE);
+
+        SharedPreferences.Editor userSessionEditor = userSession.edit();
+
+        if(userSession.contains("sessionID")){
+            //Session Reauthentication code
+        }
+
+        login = findViewById(R.id.loginButton);
+        password = findViewById(R.id.loginPasswordEntryField);
+        username = findViewById(R.id.loginUsernameEntryField);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(password.getText() != null && !(password.getText().toString().isEmpty()) &&
+                        username.getText() != null && !(username.getText().toString().isEmpty())){
+                    String url = Const.SOURCE + Const.LOGIN + username.getText().toString() + "/"
+                            + password.getText().toString();
+
+                    JsonObjectRequest loginReq = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("Volley Login Auth Error:", error.toString());
+                            Toast.makeText(getApplicationContext(), R.string.login_volley_error,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }){
+
+                    };
+                }else{
+                    Toast.makeText(getApplicationContext(), R.string.login_missing_field,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- Frontend/app/src/main/java/edu/iastate/code42/utils/Const.java
+++ Frontend/app/src/main/java/edu/iastate/code42/utils/Const.java
@@ -2,6 +2,7 @@ package edu.iastate.code42.utils;
 
 public class Const {
 	public static final String SOURCE = "";
+	public static final String LOGIN = "";
 	public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
 	public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
 	public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
