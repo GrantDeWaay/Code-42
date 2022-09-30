12c42c08d357155785a161cf8ab9a966c2214d09
Grant <grantdewaay@gmail.com>
2022-09-30 11:06:53 -0500

    Volley Setup assignment page

--- Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
+++ Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
@@ -7,19 +7,46 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class AssignmentCreateActivity extends AppCompatActivity {
     private Button btnClick;
     private EditText textE;
+    private TextView textx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_create);
         btnClick = findViewById(R.id.button);
         textE = findViewById(R.id.enterText);
+        textx = findViewById(R.id.textView);
         btnClick.setOnClickListener(v -> {
+            makeJsonObjReq();
             Log.i("Click", textE.getText().toString());
             // Do something in response to button click
         });
+    }
+
+    private void makeJsonObjReq() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.56.1:8080/HelloWorld?username=Andrew";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    textx.setText("Response is: " + response);
+                }, error -> {
+                    textx.setText("That didn't work!: " + error);
+                });
+        queue.add(stringRequest);
     }
 }
