e412dd9e856217e6594d4fb524c7526670a34191
Nolan Brown <nbrown3@iastate.edu>
2022-09-30 11:37:12 -0500

    fixed dumb bug in session token generation

--- Backend/Spring/src/main/java/coms309/controller/login/LoginController.java
+++ Backend/Spring/src/main/java/coms309/controller/login/LoginController.java
@@ -3,12 +3,12 @@ package coms309.controller.login;
 import coms309.database.dataobjects.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-
 import java.util.Random;
 
-
+@RestController
 public class LoginController {
 
     HashMap<String, String> sessionTokens = new HashMap<String, String>();
@@ -23,18 +23,27 @@ public class LoginController {
             char c = (char) (33 + r.nextInt(93)); //33-126
             sb.append(c);
         }
+        String s = sb.toString();
         sb.setLength(0);
-        return sb.toString();
+        return s;
     }
 
     @GetMapping("/login{username}{password}")
-    public @ResponseBody String studentLogin(String username, String password) {
+    public @ResponseBody
+    String studentLogin(String username, String password) {
         User u = new User(); // Instead pull user from database TODO
+        u.setUsername(username);
         if (u.getUsername().equals(username)) {
-            String token = sessionTokens.put(u.getUsername(), generateSessionToken());
+            String token = generateSessionToken();
+            sessionTokens.put(u.getUsername(), token);
             return "Login Success: " + token;
         } else {
             return "Username is not in database. Please sign up";
         }
+    }
+
+    @GetMapping("/")
+    public String serverTest() {
+        return "It's working!";
     }
 }
