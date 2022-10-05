d77cb873243773b750194499bc060563fd00d0a8
Grant <grantdewaay@gmail.com>
2022-10-05 11:21:44 -0500

    Branch Merging

--- Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
+++ Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
@@ -1,10 +1,11 @@
 package edu.iastate.code42;
 
+import static edu.iastate.code42.utils.Const.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +13,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
+import edu.iastate.code42.app.AppController;
 
 public class AssignmentCreateActivity extends AppCompatActivity {
     private Button btnClick;
@@ -28,8 +27,8 @@ public class AssignmentCreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_create);
         btnClick = findViewById(R.id.button);
-        textE = findViewById(R.id.enterText);
-        textx = findViewById(R.id.textView);
+        textE = findViewById(R.id.assignmentNameEnter);// assignment name
+        textx = findViewById(R.id.textx);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lang_array, android.R.layout.simple_spinner_item);
@@ -45,16 +44,13 @@ public class AssignmentCreateActivity extends AppCompatActivity {
 
     private void makeJsonObjReq() {
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.56.1:8080/HelloWorld?username=Andrew";
-
-// Request a string response from the provided URL.
+        String url = HELLO_WORLD + "?username=" + textE.getText();
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 response -> {
                     textx.setText("Response is: " + response);
                 }, error -> {
                     textx.setText("That didn't work!: " + error);
                 });
-        queue.add(stringRequest);
+        AppController.getInstance().addToRequestQueue(stringRequest);
     }
 }
