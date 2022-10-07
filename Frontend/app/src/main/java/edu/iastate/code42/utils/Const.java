a1f7a6cf5c8cca0d3c670ccdb603b50a89534957
Grant <grantdewaay@gmail.com>
2022-10-07 12:36:17 -0500

    Finalizing Assignment Creation Page roundtrip, with some features of the assignment work page started

--- Frontend/app/src/main/java/edu/iastate/code42/utils/Const.java
+++ Frontend/app/src/main/java/edu/iastate/code42/utils/Const.java
@@ -1,7 +1,7 @@
 package edu.iastate.code42.utils;
 
 public class Const {
-	public static final String SOURCE = "https://a6629e8b-03c4-4b87-882c-d92549b52f9e.mock.pstmn.io/";
+	public static final String SOURCE = "http://coms-309-028.class.las.iastate.edu:8080/assignment/create";
 	public static final String LOGIN = "users/login/";
 	public static final String SESSION = "users/session/";
 }
