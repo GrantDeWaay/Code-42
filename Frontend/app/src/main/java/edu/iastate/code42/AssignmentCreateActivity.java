c8455649129acce1fb5dc928714a214eb46a572a
Grant <grantdewaay@gmail.com>
2022-10-07 12:39:04 -0500

    Revert "Finalizing Assignment Creation Page roundtrip, with some features of the assignment work page started"
    This reverts commit a1f7a6cf5c8cca0d3c670ccdb603b50a89534957.

--- Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
+++ Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
@@ -1,11 +1,9 @@
 package edu.iastate.code42;
 
-import static java.security.AccessController.getContext;
 import static edu.iastate.code42.utils.Const.*;
 
 import androidx.appcompat.app.AppCompatActivity;
-import org.json.JSONException;
-import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -13,29 +11,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import edu.iastate.code42.app.AppController;
 
 public class AssignmentCreateActivity extends AppCompatActivity {
     private Button btnClick;
-    private EditText assignmentTitle;
-    private TextView desc;
-    private TextView problem;
-    private String resp;
+    private EditText textE;
+    private TextView textx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_create);
         btnClick = findViewById(R.id.button);
-        assignmentTitle = findViewById(R.id.assignmentNameEnter);// assignment name
-        desc = findViewById(R.id.description);
-        problem = findViewById(R.id.problemStatement);
+        textE = findViewById(R.id.assignmentNameEnter);// assignment name
+        textx = findViewById(R.id.textx);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lang_array, android.R.layout.simple_spinner_item);
@@ -43,29 +36,21 @@ public class AssignmentCreateActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
         spinner.setSelection(0);
         btnClick.setOnClickListener(v -> {
-            try {
-                makeJsonObjReq();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            Log.i("Click", assignmentTitle.getText().toString());
-            Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_LONG).show();
+            makeJsonObjReq();
+            Log.i("Click", textE.getText().toString());
+            // Do something in response to button click
         });
     }
 
-    private void makeJsonObjReq() throws JSONException {
+    private void makeJsonObjReq() {
         // Instantiate the RequestQueue.
-        JSONObject obj = new JSONObject();
-        obj.put("title", assignmentTitle.getText());
-        obj.put("description", desc.getText());
-        obj.put("problemStatement", problem.getText());
-        String url = SOURCE;
-        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url,obj,
+        String url = HELLO_WORLD + "?username=" + textE.getText();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 response -> {
-                    resp = response.toString();
+                    textx.setText("Response is: " + response);
                 }, error -> {
-            resp = error.toString();
+                    textx.setText("That didn't work!: " + error);
                 });
-        AppController.getInstance().addToRequestQueue(req);
+        AppController.getInstance().addToRequestQueue(stringRequest);
     }
 }
