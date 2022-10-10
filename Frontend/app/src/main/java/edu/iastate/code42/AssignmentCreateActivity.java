5e95ba9a843141e1444f36a78986a2381ac14aff
Grant <grantdewaay@gmail.com>
2022-10-10 10:56:09 -0500

    Aftermath from Demo2

--- Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
+++ Frontend/app/src/main/java/edu/iastate/code42/AssignmentCreateActivity.java
@@ -1,9 +1,11 @@
 package edu.iastate.code42;
 
+import static java.security.AccessController.getContext;
 import static edu.iastate.code42.utils.Const.*;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -11,24 +13,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import edu.iastate.code42.app.AppController;
 
 public class AssignmentCreateActivity extends AppCompatActivity {
     private Button btnClick;
-    private EditText textE;
-    private TextView textx;
+    private EditText assignmentTitle;
+    private TextView desc;
+    private TextView problem;
+    private String resp;
+    private EditText y;
+    private TextView x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_create);
         btnClick = findViewById(R.id.button);
-        textE = findViewById(R.id.assignmentNameEnter);// assignment name
-        textx = findViewById(R.id.textx);
+        assignmentTitle = findViewById(R.id.assignmentNameEnter);// assignment name
+        desc = findViewById(R.id.description);
+        problem = findViewById(R.id.problemStatement);
+        x = findViewById(R.id.textx);
+        y = findViewById(R.id.editTextTextPersonName);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lang_array, android.R.layout.simple_spinner_item);
@@ -36,21 +47,38 @@ public class AssignmentCreateActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
         spinner.setSelection(0);
         btnClick.setOnClickListener(v -> {
-            makeJsonObjReq();
-            Log.i("Click", textE.getText().toString());
-            // Do something in response to button click
+            try {
+                makeJsonObjReq();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.i("Click", assignmentTitle.getText().toString());
+            Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_LONG).show();
         });
     }
 
-    private void makeJsonObjReq() {
+    private void makeJsonObjReq() throws JSONException {
         // Instantiate the RequestQueue.
-        String url = HELLO_WORLD + "?username=" + textE.getText();
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        JSONObject obj = new JSONObject();
+        obj.put("title", assignmentTitle.getText());
+        obj.put("description", desc.getText());
+        obj.put("problemStatement", problem.getText());
+        String url = SOURCE;
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url,obj,
                 response -> {
-                    textx.setText("Response is: " + response);
+                    resp = response.toString();
                 }, error -> {
-                    textx.setText("That didn't work!: " + error);
-                });
-        AppController.getInstance().addToRequestQueue(stringRequest);
+            resp = error.toString();
+        });
+        AppController.getInstance().addToRequestQueue(req);
+
+        String urlx = sss + y.getText();
+        JsonObjectRequest pop = new JsonObjectRequest(Request.Method.GET, urlx, null,
+                response -> {
+                    x.setText( response.toString());
+                }, error -> {
+            x.setText( error.toString());
+        });
+        AppController.getInstance().addToRequestQueue(pop);
     }
 }
--- Frontend/app/src/main/java/edu/iastate/code42/utils/Const.java
+++ Frontend/app/src/main/java/edu/iastate/code42/utils/Const.java
@@ -1,7 +1,9 @@
 package edu.iastate.code42.utils;
 
 public class Const {
-	public static final String SOURCE = "https://a6629e8b-03c4-4b87-882c-d92549b52f9e.mock.pstmn.io/";
+	public static final String PYTHON = "http://192.168.56.1:5000/";
+	public static final String sss = "http://coms-309-028.class.las.iastate.edu:8080/assignment/";
+	public static final String SOURCE = "http://coms-309-028.class.las.iastate.edu:8080/assignment/create";
 	public static final String LOGIN = "users/login/";
 	public static final String SESSION = "users/session/";
 }
