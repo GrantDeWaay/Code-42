bdf40f22d6638e98e2d1bea82c5ed1eb89230b31
Nolan Brown <nbrown3@iastate.edu>
2022-10-26 15:13:29 -0500

    added user logout

--- Backend/Spring/src/main/java/coms309/controller/token/UserTokens.java
+++ Backend/Spring/src/main/java/coms309/controller/token/UserTokens.java
@@ -31,4 +31,20 @@ public class UserTokens {
         return isStudent(token) || isTeacher(token) || isAdmin(token);
     }
 
+    public static boolean removeToken(String token) {
+        if (studentTokens.containsKey(token)) {
+            studentTokens.remove(token);
+            return true;
+        }
+        if (teacherTokens.containsKey(token)) {
+            teacherTokens.remove(token);
+            return true;
+        }
+        if (adminTokens.containsKey(token)) {
+            adminTokens.remove(token);
+            return true;
+        }
+        return false;
+    }
+
 }
