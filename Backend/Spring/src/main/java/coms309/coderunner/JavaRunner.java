b04fb545d21dcea7dc7fd4ece84959279fd08ca7
Nolan Brown <nbrown3@iastate.edu>
2022-11-04 00:20:31 -0500

    fixed incorrent java run command

--- Backend/Spring/src/main/java/coms309/coderunner/JavaRunner.java
+++ Backend/Spring/src/main/java/coms309/coderunner/JavaRunner.java
@@ -18,8 +18,7 @@ public class JavaRunner extends CodeRunner {
 
     @Override
     public boolean compile() throws IOException {
-        Process process = Runtime.getRuntime().exec("javac " + testFolder + "/" + fileName);
-
+        Process process = Runtime.getRuntime().exec("javac " + testFolder + "/" + fileName + " -d " + testFolder + "/out");
         try {
             if (process.waitFor(5, TimeUnit.SECONDS)) {
                 return process.exitValue() == 0;
@@ -33,25 +32,29 @@ public class JavaRunner extends CodeRunner {
     @Override
     public boolean run() throws IOException {
         String className = fileName.substring(0, fileName.indexOf('.'));
-        Process process = Runtime.getRuntime().exec("java " + testFolder + "/" + className);
+        Process process = Runtime.getRuntime().exec("java -cp " + testFolder + "/out " + className);
 
         stdout = process.getInputStream();
+        stderr = process.getErrorStream();
 
         long startTime = System.currentTimeMillis();
 
         byte[] buff = new byte[1024];
 
         while(process.isAlive() && System.currentTimeMillis() - startTime < maxRuntime) {
-            // Waiting
-        }
-        
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+            while(stdout.available() > 0) {
+                int n = stdout.read(buff);
+                stdOutData = stdOutData.concat(new String(buff, 0, n));
+            }
 
-        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-            stdOutData += line;
+            while(stderr.available() > 0) {
+                int n = stderr.read(buff);
+                stdErrData = stdErrData.concat(new String(buff, 0, n));
+            }
         }
 
-        if (process.isAlive()) {
+
+        if(process.isAlive()){
             process.destroyForcibly();
             return false;
         }
