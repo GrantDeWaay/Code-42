f00e35a18f501421b6bf162ddfaeebcf26a69c19
nastark123 <vikingsiowa28@gmail.com>
2022-12-01 23:11:07 -0600

    Create skeleton controller for websockets

---
+++ Backend/Spring/src/main/java/coms309/controller/CodeRunnerWebSocketController.java
@@ -0,0 +1,41 @@
+package coms309.controller;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Controller;
+
+import coms309.api.dataobjects.ApiCodeSubmission;
+
+@Controller
+@ServerEndpoint(value = "/run/assignment/{assignmentId}/user/{userId}")
+public class CodeRunnerWebSocketController {
+    
+    @OnOpen
+    public void onOpen(Session session, @PathParam("assignmentId") long assignmentId, @PathParam("userId") long userId) {
+
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) throws IOException {
+        
+    }
+
+}
