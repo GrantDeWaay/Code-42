728ff733db93a779b0f698db109993d4f64bf321
Nolan Brown <nbrown3@iastate.edu>
2022-12-06 20:37:51 -0600

    tesing basic unit tests with ci cd

--- Backend/Spring/src/test/java/coms309/UnitTest.java
+++ Backend/Spring/src/test/java/coms309/UnitTest.java
@@ -1,4 +1,4 @@
-package com.cs309.tutorial;
+package coms309;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class TutorialApplicationTests {
+class UnitTest {
 
-	@Test
-	void contextLoads() {
-		assertEquals("","");
-	}
+    @Test
+    void contextLoads() {
+        assertEquals("lol", "lol");
+    }
 
 }
