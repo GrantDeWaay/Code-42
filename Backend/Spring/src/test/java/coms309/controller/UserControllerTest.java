ea3edece093e3c3939a2b827d99d16fd2d8f50a5
Nolan Brown <nbrown3@iastate.edu>
2022-12-06 18:44:57 -0600

    intentional failure of maven test to see if pipeline works

---
+++ Backend/Spring/src/test/java/coms309/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Test
+    void contextLoads() {
+        assertEquals("olleh", "lol");
+    }
+
+}
