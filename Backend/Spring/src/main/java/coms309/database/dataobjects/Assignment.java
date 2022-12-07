3d68c376a3a76674dafcb3ff00f323792146ff24
nastark123 <vikingsiowa28@gmail.com>
2022-12-07 14:31:14 -0600

    Fix another lazy exception

--- Backend/Spring/src/main/java/coms309/database/dataobjects/Assignment.java
+++ Backend/Spring/src/main/java/coms309/database/dataobjects/Assignment.java
@@ -42,7 +42,7 @@ public class Assignment {
     private Course course;
 
     // grades for everyone that did the assignment
-    @OneToMany(mappedBy = "assignment", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "assignment", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Grade> grades = new HashSet<>();
 
     @OneToOne(mappedBy = "assignment", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
